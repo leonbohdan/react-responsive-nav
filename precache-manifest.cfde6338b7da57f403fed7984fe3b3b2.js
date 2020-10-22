bbaa845dd14690d8fa2c377b6de993e038d30b9c
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-10-22 17:44:29 +0300

    Updates

---
+++ precache-manifest.cfde6338b7da57f403fed7984fe3b3b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc7ebf489b73ee19c0434b44100f049",
+    "url": "/react-responsive-nav/index.html"
+  },
+  {
+    "revision": "dd2bc782db8bfb309bbb",
+    "url": "/react-responsive-nav/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "290f4ae50e9b5e2f6a14",
+    "url": "/react-responsive-nav/static/js/2.eb7e8479.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-responsive-nav/static/js/2.eb7e8479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2bc782db8bfb309bbb",
+    "url": "/react-responsive-nav/static/js/main.2c15f5ca.chunk.js"
+  },
+  {
+    "revision": "de148e2e48c1d6d0ebd1",
+    "url": "/react-responsive-nav/static/js/runtime-main.d7d2cafc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-responsive-nav/static/media/logo.5d5d9eef.svg"
+  }
+]);
