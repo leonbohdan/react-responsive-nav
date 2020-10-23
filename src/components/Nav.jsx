c769e4bc9dd8627f16aeda59a43f8bb14b1669cb
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-10-23 18:06:57 +0300

    ability to open sub-menu in mobile-menu and to open burger-menu were added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,90 +1,178 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.scss';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 export default function Nav() {
-  const [isActive, setisActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [isFirstActiveDropDown, setFirstActiveDropDown] = useState(true);
+  const [isSecondActiveDropDown, setSecondActiveDropDown] = useState(true);
+  const [isThirdActiveDropDown, setThirdActiveDropDown] = useState(true);
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+      setFirstActiveDropDown(true);
+      setSecondActiveDropDown(true);
+      setThirdActiveDropDown(true);
+    }
+    
+
+    window.addEventListener("resize", handleResize);
+
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  })
 
   return (
-    <nav
-      className="navbar is-transparent"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/#">
-          <img src={logo} width="112" height="28" alt="logo" />
-        </a>
-
-        <div className="navbar-item">
-          <div className="buttons">
-            <button className="button" href="/#">
-              Sign up
-            </button>
-            <button className="button is-warning" href="/#">
-              Log in
-            </button>
-          </div>
-        </div>
-        <a
-          role="button"
-          className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          href="/#"
-          onClick={() => {
-            setisActive(!isActive);
-          }}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        className={`navbar-menu ${isActive ? "is-active" : ""}`}
+    <>
+      <nav
+        className="navbar is-transparent"
+        role="navigation"
+        aria-label="main navigation"
       >
-        <div className="navbar-end">
+        <div className="navbar-brand">
           <a className="navbar-item" href="/#">
-            One
+            <img src={logo} width="112" height="28" alt="logo" />
           </a>
-          <a className="navbar-item" href="/#">
-            Two
-          </a>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link is-arrowless" href="/#">
-              Three
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item" href="/#">
-                About
-              </a>
-              <a className="navbar-item" href="/#">
-                Jobs
-              </a>
-              <a className="navbar-item" href="/#">
-                Contact
-              </a>
+          {
+            <div className="navbar-item mobile-buttons">
+              <div className="buttons">
+                <button className="button login-button" href="/#">
+                  Log in
+                </button>
+                <button className="button sign-button" href="/#">
+                  Sign up
+                </button>
+              </div>
             </div>
-          </div>
+          }
 
-          <div className="navbar-item">
-            <div className="buttons">
-              <button className="button" href="/#">
-                Sign up
-              </button>
-              <button className="button is-warning" href="/#">
-                Log in
-              </button>
+          <a
+            role="button"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            href="/#"
+            onClick={() => {
+              setIsActive(!isActive);
+            }}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isActive ? "is-active" : ""}`}
+        >
+          <div className="navbar-end">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a
+                className="navbar-link is-arrowless"
+                href="/#"
+                onClick={() => {
+                  setFirstActiveDropDown(!isFirstActiveDropDown);
+                }}
+              >
+                One
+              </a>
+
+              <div
+                className={`navbar-dropdown ${
+                  isFirstActiveDropDown ? "is-hidden" : ""
+                }`}
+              >
+                <a className="navbar-item" href="/#">
+                  About
+                </a>
+                <a className="navbar-item" href="/#">
+                  Jobs
+                </a>
+                <a className="navbar-item" href="/#">
+                  Contact
+                </a>
+              </div>
+            </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a
+                className="navbar-link is-arrowless"
+                href="/#"
+                onClick={() => {
+                  setSecondActiveDropDown(!isSecondActiveDropDown);
+                }}
+              >
+                Two
+              </a>
+
+              <div
+                className={`navbar-dropdown ${
+                  isSecondActiveDropDown ? "is-hidden" : ""
+                }`}
+              >
+                <a className="navbar-item" href="/#">
+                  About
+                </a>
+                <a className="navbar-item" href="/#">
+                  Jobs
+                </a>
+                <a className="navbar-item" href="/#">
+                  Contact
+                </a>
+              </div>
+            </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a
+                className="navbar-link is-arrowless"
+                href="/#"
+                onClick={() => {
+                  setThirdActiveDropDown(!isThirdActiveDropDown);
+                }}
+              >
+                Three
+              </a>
+
+              <div
+                className={`navbar-dropdown ${
+                  isThirdActiveDropDown ? "is-hidden" : ""
+                }`}
+              >
+                <a className="navbar-item" href="/#">
+                  About
+                </a>
+                <a className="navbar-item" href="/#">
+                  Jobs
+                </a>
+                <a className="navbar-item" href="/#">
+                  Contact
+                </a>
+              </div>
+            </div>
+
+            <div className={`navbar-item nav-buttons`}>
+              <div className="buttons">
+                <button className="button login-button" href="/#">
+                  Log in
+                </button>
+                <button className="button sign-button" href="/#">
+                  Sign up
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
